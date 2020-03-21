9de7983dc6a1c20899ef0f4641ef78cb4b0a7015
oran <orantal01@gmail.com>
2020-03-21 19:47:34 +0200

    added movie seat booking

---
+++ script.js
@@ -0,0 +1,69 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+let ticketPrice = parseInt(movieSelect.value);
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    const movieSelected = e.target.value;
+    const movieIndex = e.target.selectedIndex;
+    ticketPrice = parseInt(movieSelected);
+    setMovieData(movieIndex, ticketPrice);
+    updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+    const seat = e.target;
+    if (seat.classList.contains('seat') && !seat.classList.contains('occupied')) {
+        seat.classList.toggle('selected');
+        updateSelectedCount();
+    }
+});
+
+// Initial count and total set
+updateSelectedCount();
